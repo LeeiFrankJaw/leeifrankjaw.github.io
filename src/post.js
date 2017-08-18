2bbabcc70f33c8d29c892a44d507b581e931e655
Lei Zhao <LeiZh26@gmail.com>
2017-08-19 02:42:52 +0800

    avoid using innerHTML

--- src/post.js
+++ src/post.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    a = document.querySelector(".validation a");
+    var a = document.querySelector(".validation a");
+    var img = document.createElement('img');
+    img.src = "http://www.w3.org/Icons/valid-xhtml10";
+    img.alt = "Valid XHTML 1.0 Strict";
+    img.width = 88;
+    img.height = 31;
     a.protocol = "https:";
-    a.innerHTML = '<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" width="88" height="31" />';
+    a.replaceChild(img, a.firstChild);
+    // a.innerHTML = '<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" width="88" height="31" />';
 });
