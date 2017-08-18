6fe019a3da0cb8ab1719c9a5e23d40ed3e2a6f6a
Lei Zhao <LeiZh26@gmail.com>
2017-08-19 02:45:54 +0800

    use https for images

--- src/post.js
+++ src/post.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var a = document.querySelector(".validation a");
     var img = document.createElement('img');
-    img.src = "http://www.w3.org/Icons/valid-xhtml10";
+    img.src = "https://www.w3.org/Icons/valid-xhtml10";
     img.alt = "Valid XHTML 1.0 Strict";
     img.width = 88;
     img.height = 31;
