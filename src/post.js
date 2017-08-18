cd1ef238cf916597855fd37fbab0a852f828501c
Lei Zhao <LeiZh26@gmail.com>
2017-08-18 21:32:52 +0800

    automate post processing

---
+++ src/post.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    a = document.querySelector(".validation a");
+    a.protocol = "https:";
+    a.innerHTML = '<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" width="88" height="31" />';
+});
