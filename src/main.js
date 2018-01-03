3af2d52bbd4bc7a8805035b9e724545acd3acfaa
Lei Zhao <LeiZh26@gmail.com>
2018-01-03 10:58:59 +0800

    finish homework for finite automata

--- src/main.js
+++ src/main.js
@@ -126,7 +126,7 @@ var LISTING = [
                 "type": "file",
                 "name": "automata/",
                 "title": "Automata Theory",
-                "author_date": "2018-01-02T14:45:55+08:00"
+                "author_date": "2018-01-03T10:58:59+08:00"
             },
             {
                 "type": "file",
